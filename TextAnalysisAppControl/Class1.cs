4b9412fd98f68ef1485d6ed3d18c18080f3477b0
feedfedfat <scottszb1987@hotmail.com>
2015-05-05 13:51:21 +1200

    v0.00
    --A text analysis application, composed of a GUI project, a back end
    control project, possibly will add a model project for data storage.
    --load a text file from local drive, perform sorting and searching
    corresponding to words length, appearance frequency and so on.

---
+++ TextAnalysisAppControl/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysisAppControl
+{
+    public class Class1
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TextAnalysisAppUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TextAnalysisAppControl;
+
+namespace TextAnalysisAppUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var obj = new TextAnalysisAppControl.Class1();
+            Assert.AreEqual(2, obj.add(1, 1));
+        }
+    }
+}
